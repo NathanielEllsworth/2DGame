6dc7282ec076c35ed1dd3e2ceeba87eab831f2c6
Nate Ellsworth <Nate Ellsworth>
2017-03-04 13:39:19 -0500

    Game window opens

--- src/com/NathanielEllsworth/main/Game.java
+++ src/com/NathanielEllsworth/main/Game.java
@@ -1,13 +1,34 @@
 package com.NathanielEllsworth.main;
 
+import java.awt.Canvas;
+
 /**
  * @author NathanielEllsworth
  *
  */
-public class Game {
+public class Game extends Canvas implements Runnable {
 	
+	private static final long serialVersionUID = 4047949324018978146L;
+	
+	
+	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9; // 12 * 9 is just for ease for a good aspect ratio
+
+	public Game(){
+		new Window(WIDTH, HEIGHT, "Lick My Nuts!", this); // 'this' referring to the game parameter
+	}
+	
+	
+	
+	public synchronized void start(){
+		
+	}
+	
+	public void run(){
+		
+	}
 	
 	public static void main(String args[]){
+		new Game(); //will call constructor
 		
 		
 	}
--- src/com/NathanielEllsworth/main/Window.java
+++ src/com/NathanielEllsworth/main/Window.java
@@ -1,12 +1,47 @@
 package com.NathanielEllsworth.main;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+/**
+ * 
+ * @author NathanielEllsworth the only purpose of this class is to just create the window of the game
+ *
+ */
 public class Window extends Canvas {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -5604853402217992119L;
-
+	
+	/**
+	 * created a JFrame which creates the frame of the game's window
+	 * 
+	 * @param width of game window
+	 * @param height of game window
+	 * @param title of the window
+	 * @param game inside of the window
+	 */
+	
+	public Window(int width, int height, String title, Game game){
+		
+		JFrame frame = new JFrame(title);
+		
+		frame.setPreferredSize(new Dimension(width, height));
+		frame.setMaximumSize(new Dimension(width, height));
+		frame.setMinimumSize(new Dimension(width, height));
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null); //not needed but by setting this to null the window will appear in the center of the screen 
+											//rather than the top left
+		frame.add(game); // here I'm adding the 'game class' into the frame
+		frame.setVisible(true); // setting it to visible so you can actually see it. Super complex concept here
+		game.start(); // running the start method that's in the game class
+		
+	}
+	
 }
