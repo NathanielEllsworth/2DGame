d1d36ffecb38781cfb427c352682348e5a89fb44
Nate Ellsworth <Nate Ellsworth>
2017-03-04 14:46:55 -0500

    added game loop

--- src/com/NathanielEllsworth/main/Game.java
+++ src/com/NathanielEllsworth/main/Game.java
@@ -12,19 +12,63 @@ public class Game extends Canvas implements Runnable {
 	
 	
 	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9; // 12 * 9 is just for ease for a good aspect ratio
+	
+
+	private Thread thread;
+	private boolean running = false;
+	
 
 	public Game(){
-		new Window(WIDTH, HEIGHT, "Lick My Nuts!", this); // 'this' referring to the game parameter
+		new Window(WIDTH, HEIGHT, "Ellsworth's First Game!", this); // 'this' referring to the game parameter
 	}
 	
 	
 	
-	public synchronized void start(){
+	public synchronized void start(){//the entire game is going to run within this thread
+		
+		thread = new Thread(this);
+		thread.start(); // this is where the thread is going to be ran
+		running = true; // is it running or is it not running
 		
 	}
 	
-	public void run(){
+	public synchronized void stop(){
 		
+		try{
+			thread.join(); // kills off the thread
+			running = false;
+		}catch(Exception e){
+			e.printStackTrace(); // runs an error bug 
+		}
+	}
+	
+	public void run(){ //here is the game loop which is like the heart beat of the game, every game has some version of a game loop
+		
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running){
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1){
+				tick();
+				delta--;
+			}
+			if(running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
 	}
 	
 	public static void main(String args[]){
