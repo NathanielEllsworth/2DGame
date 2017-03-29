b3127a20432f47649b4c8c078104a7655220b9f8
Nate Ellsworth <Nate Ellsworth>
2017-03-29 11:01:17 -0400

    Building in game shop class

---
+++ src/com/NathanielEllsworth/main/Shop.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.NathanielEllsworth.main;
+
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * @author NathanielEllsworth
+ *
+ */
+public class Shop extends MouseAdapter{
+	//Mouse Adapter allows you to add a mouse lister onto this class
+	//because I'm going to add a mouse listener onto the Shop class, It's going to look for MouseEvent (mousePressed method) every time you click the mouse 
+	
+	public Shop(){
+		
+	}
+	
+	public void render(Graphics g){
+		
+	}
+	
+	public void mousePressed(MouseEvent e){
+		
+	}
+
+}
